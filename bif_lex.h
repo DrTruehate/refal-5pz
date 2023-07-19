7a05f3b47dbbb5ee08bb90bf10a6814082b81ace
DrTruehate <frantsuzov1974@yandex.ru>
2023-07-19 15:33:41 +0300

    bif_lex.h +

--- bif_lex.h
+++ bif_lex.h
@@ -1,10 +1,9 @@
 /* flag for built in functions: requires the pointer to the function table. */
-# define BI_FADDR 1
+#define BI_FADDR  1
 
 /* structure for built in functions: */
 struct bitab {
-  char *fname;    /* name */
-  int fnumber;    /* internal number. */
-  int flags;    /* flags: */
+  char *fname;// name
+  int fnumber;// internal number.
+  int flags;  // flags:
 };
-
