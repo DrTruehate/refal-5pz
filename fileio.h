d3e9e976b5ba534c89ce2ad42aeb85e8d1555860
DrTruehate <frantsuzov1974@yandex.ru>
2023-07-20 10:58:19 +0300

    fileio.h +

--- fileio.h
+++ fileio.h
@@ -1,15 +1,8 @@
+/* FILE   I/O.    */
 
-# define FILE_LIMIT 40
-
-    /* FILE   I/O.    */
-
-# ifdef DEFINE_EXTERNALS
-
+#define FILE_LIMIT  40
+#ifdef DEFINE_EXTERNALS
   FILE *file_table[FILE_LIMIT];
-
-# else
-
+#else
   extern FILE *file_table[FILE_LIMIT];
-
-# endif
-
+#endif
